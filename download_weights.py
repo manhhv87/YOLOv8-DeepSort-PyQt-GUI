ea5dca7a687b3713bb0f0896c2e3180c9c2a9808
manhhv87 <67886698+manhhv87@users.noreply.github.com>
2024-08-30 21:46:30 +0700

    Update download_weights.py

--- download_weights.py
+++ download_weights.py
@@ -3,9 +3,9 @@ from src.utils.general import ROOT
 import os
 
 
-DETECTOR_URL = "https://drive.google.com/drive/folders/1OmxDFzY65rj5Nxtdz4S1XWT2QdrkwKZr?usp=share_link"
-POSE_URL = "https://drive.google.com/drive/folders/1XjlLCDhuuDNfYXmPo2rMr_RUSUDp2WMI?usp=share_link"
-SEG_URL = "https://drive.google.com/drive/folders/1tLPndFlSsV9SR8JKG2I2ijLuqsWkcHzt?usp=share_link"
+DETECTOR_URL = "https://drive.google.com/drive/folders/1OmxDFzY65rj5Nxtdz4S1XWT2QdrkwKZr"
+POSE_URL = "https://drive.google.com/drive/folders/1XjlLCDhuuDNfYXmPo2rMr_RUSUDp2WMI"
+SEG_URL = "https://drive.google.com/drive/folders/1tLPndFlSsV9SR8JKG2I2ijLuqsWkcHzt"
 
 gdown.download_folder(
     url=DETECTOR_URL, 
